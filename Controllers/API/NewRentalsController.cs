58115a45caa2f2c92ae57f2ef79167ca3701a6a9
LIASY032 <liasy032@mymail.unisa.edu.au>
2022-01-10 18:56:58 +1030

    delete unnecessary files

--- Controllers/API/NewRentalsController.cs
+++ Controllers/API/NewRentalsController.cs
@@ -26,10 +26,10 @@ namespace Vidly.Controllers.Api
 
             foreach (var movie in movies)
             {
-                if (movie.NumberAvailable == 0)
+                if (movie.NumberInStock == 0)
                     return BadRequest("Movie is not available.");
 
-                movie.NumberAvailable--;
+                movie.NumberInStock--;
 
                 var rental = new Rental
                 {
